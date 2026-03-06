66c1390db17fc62304d62532ffeaa150228c4310
Raj Sarkar <131342835+raj-sarkar123@users.noreply.github.com>
2026-03-06 15:15:46 +0530

    Updated frontend landing page and Tailwind setup

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://secure-task-manager-rst4.onrender.com//api/v1',
+    baseURL: 'https://secure-task-manager-rst4.onrender.com/api/v1',
 });
 
 // Add a request interceptor to include the JWT token
